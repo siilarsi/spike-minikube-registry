0ba414170de375965a46b600c4085da9129952cd
Siilarsi Lorenzen <siilarsi@gmail.com>
2022-06-07 08:51:44 +0200

    check if used tools are installed
    since shellcheck cannot be expected to be available on every system.
    Alternatively, it could also be skipped if not availble.
    
    and don't forget to attribute your stackoverflow inspiration:
    https://stackoverflow.blog/2009/06/25/attribution-required/

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+
 PROG=${0##*/}
 
 function main() {
+  assert_installed kubectl
+  assert_installed shellcheck
+
   shellcheck "$PROG"
 
   kubectl create namespace spike
+}
+
+# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+function assert_installed() {
+  _command=${1?please provide the command to assert}
+  if ! command -v "$_command" &> /dev/null; then
+    echo "${_command} is required to run ${PROG}"
+    exit 1
+  fi
 }
 
 main "$@"
