0b2cfc2462a1b2e34795285e93524cd2ed61cc52
Siilarsi Lorenzen <siilarsi@gmail.com>
2022-06-07 19:32:13 +0200

    test registry is running
    considered using shellspec or another bash/shell testing framework, but
    I opted for a simpler solution heavily inspired by something which I
    once saw github.com/Byron create. Not sure how to attribute that right
    now, or if that even makes sense beyond this comment.

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 PROG=${0##*/}
 
@@ -8,15 +8,40 @@ function main() {
   is_installed kubectl && is_installed minikube || exit 1
 
   case "${1-}" in
-    "setup") kubectl create namespace spike;;
-    "deploy") kubectl apply -f templates/;;
-    "test") test "${2-:@}";;
+    "setup") setup;;
+    "deploy") deploy;;
+    "test") _test "${2-:@}";;
+    "teardown") teardown;;
     *) help;;
   esac
 }
 
-function test() {
-  curl -s -w "%{http_code}" --resolve registry.test:80:"$(minikube ip)" http://registry.test/
+function _test() {
+  echo "--- running test suite ---"
+  (sandbox
+    (when "deploying the registry"
+      deploy &>/dev/null
+      (it "should be possible to reach the base URL"
+        expected="200"
+
+        actual=$(get_http_status_of http://registry.test/)
+
+        expect_equals "$expected" "$actual"
+      )
+    )
+  )
+}
+
+function deploy() {
+  kubectl apply -f templates/
+}
+
+function setup() {
+  kubectl create namespace spike
+}
+
+function teardown() {
+  kubectl delete namespace spike
 }
 
 function help() {
@@ -38,6 +63,47 @@ function is_installed() {
     echo "${_command} is not installed" 1>&2
     return 1
   fi
+}
+
+# https://stackoverflow.com/questions/42873285/curl-retry-mechanism
+function get_http_status_of() {
+  url=${1?please provide the url a first argument}
+  curl --retry-all-errors --retry 5 --retry-delay 0 \
+    --max-time 2 --retry-max-time 15 -s -w "%{http_code}" \
+    --resolve registry.test:80:"$(minikube ip)" \
+    -O /dev/null "$url" | sed -e 's/0*//'
+    # the sed command is an ugly hack until I figure how to get the http_code of last request only
+}
+
+
+function sandbox() {
+  trap "teardown &>/dev/null" EXIT
+  setup &>/dev/null
+}
+
+function when() {
+  _scope when "$*"
+}
+
+function it() {
+  _scope it "$*"
+}
+
+function expect_equals() {
+  expected=${1?please provide expected as the first argument}
+  actual=${2?please provide actual as the second argument}
+  (
+    trap 'if [ $? != 0 ]; then echo "--FAILED expected ${expected} got ${actual}"; fi' EXIT
+    test "$expected" = "$actual"
+  )
+}
+
+OFFSET=( )
+function _scope() {
+  name="${1:?}"
+  shift
+  echo "${OFFSET[*]:-}[${name}] $*"
+  OFFSET+=("  ")
 }
 
 main "$@"
