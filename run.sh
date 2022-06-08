88eda8eae10ec3f1c0b793ffb255ace846c9c06e
Siilarsi Lorenzen <siilarsi@gmail.com>
2022-06-08 16:05:21 +0200

    transfer images from local registry to minikube registry
    this might seem like an odd way of populating the registry we have
    deployed to minikube with images for testing, but there did not seem to
    be an easy way of doing this that was also somewhat portable.
    Otherwise it would be required to change the local dns lookup to use
    the minikube dns, but that would require changes in the local machine
    setup, which is annoying and also not necessarily straightforward if
    that machine is a GitHub runner.
    
    In order to make it possible to transfer images from one registry to
    another, digest by digest, blob by blob and manifest, the following flow
    has to be followed:
    
    GET manifest from source registry
    -> extract the digest from the layers of the manifest
    -> GET blobs by digest from source registry
    -> POST upload to destination registry
    -> extract location from response header
    -> PUT blobs to destination registry using the location
    -> PUT manifest to destination registry
    The image has now been transferred from the source to the destination
    registry.
    See:
    1. https://docs.docker.com/registry/spec/api/#get-manifest
    2. https://docs.docker.com/registry/spec/api/#pulling-an-image
    3. https://docs.docker.com/registry/spec/api/#content-digests
    4. https://docs.docker.com/registry/spec/api/#get-blob
    5. https://docs.docker.com/registry/spec/api/#initiate-blob-upload
    6. https://docs.docker.com/registry/spec/api/#put-blob-upload
    7. https://docs.docker.com/registry/spec/api/#put-manifest
    bonus: https://docs.docker.com/registry/spec/manifest-v2-2/#media-types
    
    Right now the above is handled by one function, which was put together
    step by step without tests, not ideal, and likely not very robust, but
    luckily interacting with the docker registry API is idempotent.
    Nevertheless it might make sense to port it to Golang or some other
    fitting language at some point to tests it and make it more robust.
    
    Note that annotations was added to the registry ingress to fix the
    error: "413 Entity too large" from the nginx ingress controller when
    putting large blobs to the registry.
    https://stackoverflow.com/questions/64759983/nginx-ingress-returns-413-entity-too-large

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 PROG=${0##*/}
+SOURCE_REGISTRY="localhost:5000"
+DESTINATION_REGISTRY="registry.test"
 
 function main() {
   is_installed shellcheck 2>/dev/null && shellcheck "$PROG"
@@ -9,8 +11,9 @@ function main() {
   case "${1-}" in
     "setup") setup;;
     "deploy") deploy;;
-    "test") _test "${2-:@}";;
+    "test") _test;;
     "teardown") teardown;;
+    "transfer") transfer "${2-registry}" "${3-latest}";;
     *) help;;
   esac
 }
@@ -46,12 +49,35 @@ function _test() {
       (it "should not contain any repositories"
         expected="0"
 
-        actual=$(get_body_of http://registry.test/v2/_catalog | jq '.repositories | length')
+        actual=$(_curl http://registry.test/v2/_catalog | jq '.repositories | length')
 
         expect_equals "$expected" "$actual"
       )
     )
   )
+}
+
+function transfer() {
+  repo=${1?please provide repository as the first argument}
+  tag=${2?please provide tag as the second argument}
+  echo "transferring $repo:$tag"
+  manifest=$(_curl -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+    "http://${SOURCE_REGISTRY}/v2/${repo}/manifests/${tag}")
+    echo "$manifest" | jq -r '.config.digest, .layers[].digest' \
+      | while read -r digest; do
+        location=$(_curl -iX POST "http://${DESTINATION_REGISTRY}/v2/${repo}/blobs/uploads/" \
+          | grep "Location" | cut -d ' ' -f2 | tr -d "\r")
+        _curl -o - "http://${SOURCE_REGISTRY}/v2/${repo}/blobs/${digest}" \
+          | _curl -H "content-type: application/octet-stream" \
+            --data-binary "@-" -X PUT "${location}&digest=${digest}"
+      done
+  echo "$manifest" \
+  | _curl -H "content-type: application/vnd.docker.distribution.manifest.v2+json" \
+    --data-binary "@-" -X PUT "http://${DESTINATION_REGISTRY}/v2/${repo}/manifests/${tag}"
+}
+
+function _curl() {
+  curl -s --resolve "$DESTINATION_REGISTRY":80:"$(minikube ip)" "$@"
 }
 
 function deploy() {
@@ -78,6 +104,8 @@ COMMANDS
   setup     setup the k8s registry environment
   deploy    deploys the registry to k8s
   test      run the test suite against the registry
+  teardown  cleanup the k8s registry environment
+  transfer  transfer image from the host registry to the deployed minikube registry
 
 NOTE
 Your current kubectl context is "$(kubectl config current-context)"
@@ -96,16 +124,10 @@ function is_installed() {
 # https://stackoverflow.com/questions/42873285/curl-retry-mechanism
 function get_http_status_of() {
   url=${1?please provide the url a first argument}
-  curl --retry-all-errors --retry 5 --retry-delay 0 \
-    --max-time 2 --retry-max-time 15 -s -w "%{http_code}" \
-    --resolve registry.test:80:"$(minikube ip)" \
+  _curl --retry-all-errors --retry 5 --retry-delay 0 \
+    --max-time 2 --retry-max-time 15 -w "%{http_code}" \
     -O /dev/null "$url" | sed -e 's/0*//'
     # the sed command is an ugly hack until I figure how to get the http_code of last request only
-}
-
-function get_body_of() {
-  url=${1?please provide the url a first argument}
-  curl -s --resolve registry.test:80:"$(minikube ip)" "$url"
 }
 
 function sandbox() {
